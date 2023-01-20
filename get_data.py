9ea846302e8483ac1ae7c4ff026a63ebdae8d142
alexeynichnikov <a.nichnikov@gmail.com>
2023-01-20 17:42:40 +0300

    data and scripts upload

--- get_data.py
+++ get_data.py
@@ -1,8 +1,10 @@
 import os
+import re
 import json
 from datetime import datetime
 from src.storage import DataFromDB
 from src.config import PROJECT_ROOT_DIR
+import pandas as pd
 
 with open(os.path.join(PROJECT_ROOT_DIR, "data", "statistics_parameters.json")) as st_f:
     stat_prmtrs = json.load(st_f)
@@ -14,5 +16,12 @@ db_con = DataFromDB(**db_credentials)
 today = datetime.today().strftime('%Y-%m-%d')
 rows = db_con.get_rows(1, today)
 
-print(rows[:10])
-print(len(rows))
+patterns = [r"\xa0"]
+# patterns_ = re.compile("|".join([r"\b" + tx + r"\b" for tx in patterns])
+patterns_ = re.compile("|".join([pt for pt in patterns]))
+
+txts = [patterns_.sub("", r.ShortAnswerText) for r in rows[:10]]
+txts_df = pd.DataFrame(txts, columns=["ShortAnswer"])
+
+print(txts_df)
+txts_df.to_csv(os.path.join("data", "short_answers.csv"))
